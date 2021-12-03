012673640762709cda21871064dbd29675f4770a
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-12-03 16:54:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Hello
+    <Jia/>
+  </div>
+</template>
+
+<script>
+import Jia from './components/Starts.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Jia
+  }
+}
+</script>
