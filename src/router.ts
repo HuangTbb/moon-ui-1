97072618fa7e560dcd6fe6a456e9553d11c81141
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-12-06 09:14:54 +0800

    路由切换之后关闭aside

---
+++ src/router.ts
@@ -0,0 +1,24 @@
+import SwitchDemo from "./components/SwitchDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import ButtonDemo from "./components/ButtonDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+const history = createWebHashHistory();
+export const router = createRouter({
+  history,
+  routes: [
+    { path: "/", component: Home },
+    {
+      path: "/doc",
+      component: Doc,
+      children: [
+        { path: "switch", component: SwitchDemo },
+        { path: "button", component: ButtonDemo },
+        { path: "dialog", component: DialogDemo },
+        { path: "tabs", component: TabsDemo },
+      ],
+    },
+  ],
+});
