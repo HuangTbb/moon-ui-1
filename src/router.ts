267593f7420c1a5433d0fad27727871d166120e6
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-12-06 09:33:55 +0800

    基本完成网站骨架

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,11 @@
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
 import SwitchDemo from "./components/SwitchDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
-import { createWebHashHistory, createRouter } from "vue-router";
-import Home from "./views/Home.vue";
-import Doc from "./views/Doc.vue";
+import DocDemo from "./components/DocDemo.vue";
 const history = createWebHashHistory();
 export const router = createRouter({
   history,
@@ -14,6 +15,7 @@ export const router = createRouter({
       path: "/doc",
       component: Doc,
       children: [
+        { path: "", component: DocDemo },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
