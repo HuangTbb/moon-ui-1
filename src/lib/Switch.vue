55f5d16e9e7cd601577ee89ed51e8a3b560da6c5
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-12-06 16:47:08 +0800

    switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -21,7 +21,7 @@ $h2: $h - 4px;
 button {
   height: $h;
   width: $h * 2;
-  background: gray;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
 }
@@ -33,12 +33,23 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
-  transition: left 250ms;
+  transition: all 250ms;
 }
 button.checked {
-  background: blue;
+  background: #1890ff;
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:active {
+  > span {
+    width: $h2 + 4px;
+  }
+}
+button.checked:active {
+  > span {
+    width: $h2 + 4px;
+    margin-left: -4px;
+  }
 }
 </style>
