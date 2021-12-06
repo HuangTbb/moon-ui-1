d9e9ae3f9e3657965e222645dd35677f09433ee0
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-12-06 22:00:27 +0800

    Button支持size属性，添加css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,12 +1,24 @@
 <template>
-  <button class="gulu-button" :class="{ [`gulu-theme-${theme}`]: theme }">
+  <button class="gulu-button" :class="classes">
     <slot />
   </button>
 </template>
 <script lang="ts">
+import { computed } from "vue";
 export default {
   props: {
     theme: { type: String, default: "button" },
+    size: { type: String, default: "normal" },
+  },
+  setup(props) {
+    const { theme, size } = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+      };
+    });
+    return { classes };
   },
 };
 </script>
@@ -44,6 +56,17 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.gulu-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.gulu-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
   &.gulu-theme-link {
     border-color: transparent;
     box-shadow: none;
