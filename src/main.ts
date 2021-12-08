0c35dd825232c26c548de70e683598c603cd91e1
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-12-08 17:15:00 +0800

    删除无用文件

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import "./index.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import svgIcon from "./icons/index.vue";
 import { router } from "./router";
 const app = createApp(App);
 app.use(router);
-app.component("svg-icon", svgIcon);
 app.mount("#app");
--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-// import { defineConfig } from "vite";
-// import { createSvg } from "./src/icons/index";
-
-// export default defineConfig({
-//   plugins: [vue(), createSvg("./src/icons/svg/")],
-// });
