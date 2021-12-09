7319a09d8c5223f951b323a8935c72fa094fa03d
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-12-09 21:20:58 +0800

    消除build 之后不加载 md 文件的bug，消除Dialog组件的警告

--- src/components/Dialog2.demo.vue
+++ src/components/Dialog2.demo.vue
@@ -15,13 +15,13 @@ export default {
   setup() {
     const showDialog = () => {
       openDialog({
-        title: h('strong', {}, '标题'),
-        content: "你好",
+        title() {return h('strong', {}, '标题')},
+        content(){return  "你好"},
         ok() {
           console.log("ok");
         },
         cancel() {
-          console.log("cancel");
+          console.log("cancelK");
         },
       });
     };
