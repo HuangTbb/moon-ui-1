299b4c9b15849950dc71d5fcdc2d57e28218a27e
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-12-10 13:04:18 +0800

    更改gitee部署文件

--- deploy-gitee.sh
+++ deploy-gitee.sh
@@ -2,10 +2,9 @@ rm -rf dist &&
 yarn build &&
 cd dist &&
 git init &&
-touch README.md &&
 git add . &&
 git commit -m "update" &&
 git remote add origin git@gitee.com:huangtobe/icecream_ui.git &&
 git push -f -u origin master &&
 cd -
-echo https://huangtbb.github.io/icecream-ui/index.html
+echo https://huangtobe.gitee.io/icecream_ui/index.html
